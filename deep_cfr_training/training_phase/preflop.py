b33934930e4c95c3bb79911c1f5c6ca0bca702ab
sean2474 <47488868+sean2474@users.noreply.github.com>
2026-03-19 16:41:50 -0400

    .

--- deep_cfr_training/training_phase/preflop.py
+++ deep_cfr_training/training_phase/preflop.py
@@ -8,11 +8,12 @@ from deep_cfr_training.stretegy import Preflop
 
 def train(preflop_model: Preflop, n_iters: int = 200_000,
           save_path: str = None, n_workers: int = 1,
-          discard_sims: int = 20):
+          discard_sims: int = 20, checkpoint_path: str = None):
     """Train preflop CFR standalone."""
     preflop_model.train(
         n_iters=n_iters,
         save_path=save_path,
         n_workers=n_workers,
         discard_sims=discard_sims,
+        checkpoint_path=checkpoint_path,
     )
